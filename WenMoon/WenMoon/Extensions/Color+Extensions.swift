c1dd639967b4e56728e1b3eba1707edc45ed3b68
Artur Tkachenko <ar.tkachenko97@gmail.com>
2024-10-28 21:15:16 +0100

    Add app icon and configure launch screen

--- WenMoon/WenMoon/Extensions/Color+Extensions.swift
+++ WenMoon/WenMoon/Extensions/Color+Extensions.swift
@@ -8,5 +8,6 @@
 import SwiftUI
 
 extension Color {
-    static let wmPink = Color(red: 1, green: 212 / 255, blue: 212 / 255)
+    static let wmBlack = Color("wmBlack")
+    static let wmPink = Color("wmPink")
 }
